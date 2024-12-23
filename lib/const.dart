86848566a60081c572812b5ae90740f5a6336bed
PhucTan1705 <phuctan1705@gmail.com>
2024-12-23 16:35:30 +0700

    api_tutorial

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,1 @@
-//String baseUrl = 'http://192.168.1.75:1337';
-//String baseUrl = 'http://192.168.1.8:1337';
-//String baseUrl = 'http://192.168.1.4:1337';
-//Truong
-//String baseUrl = 'http://172.16.2.132:1337';
-//String baseUrl = 'http://172.20.222.214:1337';
 String baseUrl = 'http://172.16.0.237:1337';
